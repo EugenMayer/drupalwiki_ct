b864cd7cbe55d5905a3aef4541e79531cbd82e7b
em <eugen.mayer@kontextwork.de>
2013-04-02 01:49:39 +0200

    fixed the way ctools ajaxify is triggered

--- js/drupalwiki_ct_commands.js
+++ js/drupalwiki_ct_commands.js
@@ -1,11 +1,11 @@
 Drupal.CTools.AJAX.commands.ct_dialog = function (data) {
   var path = data.path;
-  Drupal.Dialog.clickAjaxLink.apply($('<a href="' + path + '">dummy</a>')).click();
+  $(Drupal.Dialog.clickAjaxLink.apply($('<a href="' + path + '">dummy</a>'))).click();
 };
 
 Drupal.CTools.AJAX.commands.ct_ajax = function (data) {
   var path = data.path;
-  Drupal.CTools.AJAX.clickAJAXLink.apply($('<a href="' + path + '">dummy</a>')).click();
+  $(Drupal.CTools.AJAX.clickAJAXLink.apply($('<a href="' + path + '">dummy</a>'))).click();
 };
 
 //ajax submission
